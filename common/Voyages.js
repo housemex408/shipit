a2f2bff08c08833d7f0515d1d5b7eeecc882c5d1
Jose Alvarado <jalvarado80@gmail.com>
2017-05-22 19:35:58 -0700

    adding test data

--- common/Voyages.js
+++ common/Voyages.js
@@ -46,19 +46,34 @@ class Voyages  {
     //TODO: sort list first
     //Now build the list
 
-    portCalls.forEach((value, index) =>
-    {
-      this.ports.push(new Port(index, value.port, null));
-    });
+    // portCalls.forEach((value, index) =>
+    // {
+    //   this.ports.push(new Port(index, value.port, null));
+    // });
+    //
+    // this.ports.forEach((value, index, array) =>
+    // {
+    //   for(let v = index + 1; v < array.length; v++)
+    //   {
+    //     const nbr = this.ports[v];
+    //     value.neighbors = new Neighbor(nbr.portNumber, value.neighbors);
+    //   }
+    // });
 
-    this.ports.forEach((value, index, array) =>
-    {
-      for(let v = index + 1; v < array.length; v++)
-      {
-        const nbr = this.ports[v];
-        value.neighbors = new Neighbor(nbr.portNumber, value.neighbors);
-      }
-    });
+
+    this.ports.push(new Port(0, "HKHKG", null));
+    this.ports.push(new Port(1, "SGSIN", null));
+    this.ports.push(new Port(2, "USLAX", null));
+    this.ports.push(new Port(3, "USOAK", null));
+    this.ports.push(new Port(4, "COBOG", null));
+
+    this.ports[0].neighbors = new Neighbor(1, this.ports[0].neighbors);
+    this.ports[0].neighbors = new Neighbor(2, this.ports[0].neighbors);
+    this.ports[0].neighbors = new Neighbor(3, this.ports[0].neighbors);
+    this.ports[1].neighbors = new Neighbor(2, this.ports[1].neighbors);
+    this.ports[1].neighbors = new Neighbor(3, this.ports[1].neighbors);
+    this.ports[2].neighbors = new Neighbor(3, this.ports[2].neighbors);
+    this.ports[2].neighbors = new Neighbor(4, this.ports[2].neighbors);
 
     //this.executeDepthFirstTraversal();
     this.print();
