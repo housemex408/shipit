c8a776d0b9707a2aa1287bc9b7a35d828545cf67
Jose Alvarado <jalvarado80@gmail.com>
2017-05-23 00:15:15 -0700

    adding transhipments

--- common/Voyages.js
+++ common/Voyages.js
@@ -5,36 +5,134 @@ class Voyages  {
     this.ports = [];
   }
 
-  // executeDepthFirstTraversal()
-  // {
-  //   let visited = new Array(this.ports.length);
-  //
-  //   for (let v = 0; v < visited.length; v++) {
-  //     if (!visited[v]) {
-  //       console.log(`\nSTARTING AT ${this.ports[v].name}`);
-  //       this.depthFirstTraversal(v, visited);
-  //     }
-  //   }
-  // }
-  //
-  // depthFirstTraversal(v, visited)
-  // {
-  //   visited[v] = true;
-  //   console.log(`visiting ${this.ports[v].name}`);
-  //
-  //   for (let nbr = this.ports[v].neighbors; nbr != null; nbr = nbr.next) {
-  //     if (!visited[nbr.portNumber]) {
-  //       console.log(`\n ${this.ports[v].name} -- ${this.ports[nbr.portNumber].name}`);
-  //       this.depthFirstTraversal(nbr.portNumber, visited);
-  //     }
-  //   }
-  // }
+  executeDepthFirstTraversal()
+  {
+    let visited = new Array(this.ports.length);
+
+    for (let v = 0; v < visited.length; v++) {
+      if (!visited[v]) {
+        console.log(`\nSTARTING AT ${this.ports[v].name}`);
+        this.depthFirstTraversal(v, visited);
+      }
+    }
+  }
+
+  depthFirstTraversal(v, visited)
+  {
+    visited[v] = true;
+    console.log(`visiting ${this.ports[v].name}`);
+
+    for (let nbr = this.ports[v].next; nbr != null; nbr = nbr.next) {
+      if (!visited[nbr.portNumber]) {
+        console.log(`\n ${this.ports[v].name} -- ${this.ports[nbr.portNumber].name}`);
+        this.depthFirstTraversal(nbr.portNumber, visited);
+      }
+    }
+  }
+
+  extractRoutes(portCalls)
+  {
+    const results = [];
+
+    const routeIds = new Set();
+
+    portCalls.forEach((value)=>{
+      routeIds.add(value.routeId);
+    });
+
+    for (let item of routeIds.values())
+    {
+      const routeItems = portCalls.filter((i) => i.routeId == item);
+      results.push(routeItems);
+    }
+
+    return results;
+  }
+
+  generateAllRoutes(portCalls)
+  {
+    const results = [];
+    portCalls.forEach((route, index, arr) => {
+      route.forEach((port, index, arr) => {
+        for(let v = index + 1; v < arr.length; v++)
+        {
+          const next = arr[v];
+          results.push({ from: port, to: next })
+        }
+      })
+    });
+    return results;
+  }
+
+  intersection(setA, setB) {
+    let intersection = new Set();
+    for (let elem of setB) {
+      if (setA.has(elem)) {
+        intersection.add(elem);
+      }
+    }
+    return intersection;
+  }
+
+  findTranshipments(portCalls)
+  {
+    //find common ports
+    const map = new Map();
+    const results = [];
+    const transhipments = [];
+
+    portCalls.forEach((value) => {
+      let count = 0;
+
+      if(value.from.etd == null)
+        return;
+
+      if(map.has(value.from.port))
+      {
+        count = map.get(value.from.port);
+      }
+      map.set(value.from.port, ++count);
+
+    });
+
+    for (let key of map.keys()) {
+      let value = map.get(key);
+      if(value > 1) {
+        portCalls.forEach((i) => {
+          if(i.from.port == key)
+            results.push(i);
+        });
+      }
+    }
+
+    results.sort((a, b) => {
+      let dateA = new Date(a.from.etd);
+      let dateB = new Date(b.from.etd);
+      if(dateA < dateB)
+        return -1;
+      else
+        return 1;
+    })
+
+
+    for(let i = 1; i < results.length; i++)
+    {
+      //find index in previous array
+      //see if there is a next element in current array
+      //connect the two
+      let current = results[i - 1];
+      let next = results[i];
+      transhipments.push({ from: current.from, to: next.to });
+    }
+
+    return transhipments;
+  }
 
   print()
   {
     for (let v = 0; v < this.ports.length; v++) {
       console.log(this.ports[v].name);
-      for (let nbr = this.ports[v].neighbors; nbr != null; nbr = nbr.next) {
+      for (let nbr = this.ports[v].next; nbr != null; nbr = nbr.next) {
         console.log(` --> ${this.ports[nbr.portNumber].name}`);
       }
     console.log("\n");
@@ -48,7 +146,7 @@ class Voyages  {
 
     // portCalls.forEach((value, index) =>
     // {
-    //   this.ports.push(new Port(index, value.port, null));
+    //   this.ports.push(new Port(index, value.port));
     // });
     //
     // this.ports.forEach((value, index, array) =>
@@ -56,7 +154,7 @@ class Voyages  {
     //   for(let v = index + 1; v < array.length; v++)
     //   {
     //     const nbr = this.ports[v];
-    //     value.neighbors = new Neighbor(nbr.portNumber, value.neighbors);
+    //     value.next = new Neighbor(nbr.portNumber, value.next);
     //   }
     // });
 
@@ -67,13 +165,13 @@ class Voyages  {
     this.ports.push(new Port(3, "USOAK", null));
     this.ports.push(new Port(4, "COBOG", null));
 
-    this.ports[0].neighbors = new Neighbor(1, this.ports[0].neighbors);
-    this.ports[0].neighbors = new Neighbor(2, this.ports[0].neighbors);
-    this.ports[0].neighbors = new Neighbor(3, this.ports[0].neighbors);
-    this.ports[1].neighbors = new Neighbor(2, this.ports[1].neighbors);
-    this.ports[1].neighbors = new Neighbor(3, this.ports[1].neighbors);
-    this.ports[2].neighbors = new Neighbor(3, this.ports[2].neighbors);
-    this.ports[2].neighbors = new Neighbor(4, this.ports[2].neighbors);
+    this.ports[0].next = new Neighbor(1, this.ports[0].next);
+    this.ports[0].next = new Neighbor(2, this.ports[0].next);
+    this.ports[0].next = new Neighbor(3, this.ports[0].next);
+    this.ports[1].next = new Neighbor(2, this.ports[1].next);
+    this.ports[1].next = new Neighbor(3, this.ports[1].next);
+    this.ports[2].next = new Neighbor(3, this.ports[2].next);
+    this.ports[2].next = new Neighbor(4, this.ports[2].next);
 
     //this.executeDepthFirstTraversal();
     this.print();
@@ -83,11 +181,11 @@ class Voyages  {
 
 
 class Port {
-  constructor(portNumber, name, neighbors)
+  constructor(portNumber, name)
   {
     this.portNumber = portNumber;
     this.name = name;
-    this.ports = neighbors;
+    this.next = null;
   }
 }
 
--- test/portCall.js
+++ test/portCall.js
@@ -47,4 +47,276 @@ describe('VoyagesGraph', function() {
 
     done();
   });
+
+  it('extractRoutes_groupOnRouteId_generateGroups', function(done) {
+    //arrange
+    const portCalls = [
+      {
+        "id":1,
+        "vessel": "USS Harpoon",
+        "routeId": 1,
+        "port": "HKHKG",
+        "eta": null,
+        "etd": "2016-01-03 00:00:00"
+      },
+      {
+        "id":2,
+        "vessel": "USS Harpoon",
+        "routeId": 1,
+        "port": "SGSIN",
+        "eta": "2016-01-06 00:00:00",
+        "etd": "2016-01-09 00:00:00"
+      },
+      {
+        "id":3,
+        "vessel": "USS Harpoon",
+        "routeId": 1,
+        "port": "USLAX",
+        "eta": "2016-01-12 00:00:00",
+        "etd": "2016-01-15 00:00:00"
+      },
+      {
+        "id":4,
+        "vessel": "USS Harpoon",
+        "routeId": 1,
+        "port": "USOAK",
+        "eta": "2016-01-18 00:00:00",
+        "etd": null
+      },
+      {
+        "id":5,
+        "vessel": "USS Starboard",
+        "routeId": 2,
+        "port": "HKHKG",
+        "eta": null,
+        "etd": "2016-01-04 00:00:00"
+      },
+      {
+        "id":6,
+        "vessel": "USS Starboard",
+        "routeId": 2,
+        "port": "USLAX",
+        "eta": "2016-01-06 00:00:00",
+        "etd": "2016-01-08 00:00:00"
+      },
+      {
+        "id":7,
+        "vessel": "USS Starboard",
+        "routeId": 2,
+        "port": "USOAK",
+        "eta": "2016-01-10 00:00:00",
+        "etd": "2016-01-12 00:00:00"
+      },
+      {
+        "id":8,
+        "vessel": "USS Starboard",
+        "routeId": 2,
+        "port": "SGSIN",
+        "eta": "2016-01-14 00:00:00",
+        "etd": "2016-01-16 00:00:00"
+      },
+      {
+        "id":9,
+        "vessel": "USS Starboard",
+        "routeId": 2,
+        "port": "HKHKG",
+        "eta": "2016-01-18 00:00:00",
+        "etd": "2016-01-20 00:00:00"
+      },
+      {
+        "id":10,
+        "vessel": "USS Starboard",
+        "routeId": 2,
+        "port": "USLAX",
+        "eta": "2016-01-22 00:00:00",
+        "etd": null
+      },
+      {
+        "id":11,
+        "vessel": "HMS Port",
+        "routeId": 3,
+        "port": "USOAK",
+        "eta": null,
+        "etd": "2016-01-15 00:00:00"
+      },
+      {
+        "id":12,
+        "vessel": "HMS Port",
+        "routeId": 3,
+        "port": "HKHKG",
+        "eta": "2016-01-20 00:00:00",
+        "etd": "2016-01-25 00:00:00"
+      },
+      {
+        "id":13,
+        "vessel": "HMS Port",
+        "routeId": 3,
+        "port": "SGSIN",
+        "eta": "2016-01-30 00:00:00",
+        "etd": null
+      },
+      {
+        "id":14,
+        "vessel": "USS Harpoon",
+        "routeId": 4,
+        "port": "USOAK",
+        "eta": null,
+        "etd": "2016-01-24 00:00:00"
+      },
+      {
+        "id":15,
+        "vessel": "USS Harpoon",
+        "routeId": 4,
+        "port": "USLAX",
+        "eta": "2016-01-25 00:00:00",
+        "etd": "2016-01-27 00:00:00"
+      },
+      {
+        "id":16,
+        "vessel": "USS Harpoon",
+        "routeId": 4,
+        "port": "HKHKG",
+        "eta": "2016-01-28 00:00:00",
+        "etd": "2016-01-30 00:00:00"
+      },
+      {
+        "id":17,
+        "vessel": "USS Harpoon",
+        "routeId": 4,
+        "port": "SGSIN",
+        "eta": "2016-02-01 00:00:00",
+        "etd": null
+      }
+    ];
+
+    //act
+    const vg = new Voyages();
+    const voyages = vg.extractRoutes(portCalls);
+
+    //assert
+    assert.equal(voyages[0][0].routeId, 1);
+    assert.equal(voyages[1][0].routeId, 2);
+    assert.equal(voyages[2][0].routeId, 3);
+    assert.equal(voyages[3][0].routeId, 4);
+
+    done();
+  });
+
+  it('generateAllRoutes', function(done) {
+    //arrange
+    const portCalls = [[
+      {
+        "id":1,
+        "vessel": "USS Harpoon",
+        "routeId": 1,
+        "port": "HKHKG",
+        "eta": null,
+        "etd": "2016-01-03 00:00:00"
+      },
+      {
+        "id":2,
+        "vessel": "USS Harpoon",
+        "routeId": 1,
+        "port": "SGSIN",
+        "eta": "2016-01-06 00:00:00",
+        "etd": "2016-01-09 00:00:00"
+      },
+      {
+        "id":3,
+        "vessel": "USS Harpoon",
+        "routeId": 1,
+        "port": "USLAX",
+        "eta": "2016-01-12 00:00:00",
+        "etd": "2016-01-15 00:00:00"
+      },
+      {
+        "id":4,
+        "vessel": "USS Harpoon",
+        "routeId": 1,
+        "port": "USOAK",
+        "eta": "2016-01-18 00:00:00",
+        "etd": null
+      }]
+    ];
+
+    //act
+    const vg = new Voyages();
+    const voyages = vg.generateAllRoutes(portCalls);
+
+    //assert
+    //HKHKG to SGSIN
+    assert.equal(voyages[0].from.port, portCalls[0][0].port);
+    assert.equal(voyages[0].to.port, portCalls[0][1].port);
+    //HKHKG to USLAX
+    assert.equal(voyages[1].from.port, portCalls[0][0].port);
+    assert.equal(voyages[1].to.port, portCalls[0][2].port);
+    //HKHKG to USOAK
+    assert.equal(voyages[2].from.port, portCalls[0][0].port);
+    assert.equal(voyages[2].to.port, portCalls[0][3].port);
+    //SGSIN to USLAX
+    assert.equal(voyages[3].from.port, portCalls[0][1].port);
+    assert.equal(voyages[3].to.port, portCalls[0][2].port);
+    //SGSIN to USOAK
+    assert.equal(voyages[4].from.port, portCalls[0][1].port);
+    assert.equal(voyages[4].to.port, portCalls[0][3].port);
+    //USLAX to USOAK
+    assert.equal(voyages[5].from.port, portCalls[0][2].port);
+    assert.equal(voyages[5].to.port, portCalls[0][3].port);
+
+    assert.equal(voyages.length, 6);
+
+    done();
+  });
+
+  it('findTranshipments', function(done) {
+    //arrange
+    const portCalls = [
+      {
+        from: {
+          "id":3,
+          "vessel": "USS Harpoon",
+          "routeId": 1,
+          "port": "USLAX",
+          "eta": "2016-01-12 00:00:00",
+          "etd": "2016-01-15 00:00:00"
+        },
+        to: {
+          "id":4,
+          "vessel": "USS Harpoon",
+          "routeId": 1,
+          "port": "USOAK",
+          "eta": "2016-01-18 00:00:00",
+          "etd": null
+        }
+      },
+      {
+        from: {
+          "id":5,
+          "vessel": "USS Awesome",
+          "routeId": 2,
+          "port": "USLAX",
+          "eta": "2016-01-12 00:00:00",
+          "etd": "2016-01-15 06:00:00"
+        },
+        to: {
+          "id":6,
+          "vessel": "USS Awesome",
+          "routeId": 2,
+          "port": "COBOG",
+          "eta": "2016-01-18 00:00:00",
+          "etd": null
+        }
+      }
+    ];
+
+    //act
+    const vg = new Voyages();
+    const voyages = vg.findTranshipments(portCalls);
+
+    //assert
+    assert.equal(voyages[0].from, portCalls[0].from);
+    assert.equal(voyages[0].to, portCalls[1].to);
+
+    done();
+  });
 });
